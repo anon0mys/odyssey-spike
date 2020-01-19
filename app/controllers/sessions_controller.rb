8d2b6d7a1c6452a1eff5d262af23e130fb35a515
Evan Wheeler <anon0mys87@gmail.com>
2020-01-18 20:25:43 -0700

    Add users and basic auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       redirect_to welcome_path
+    else
+       redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to welcome_path
+  end
+
+  def welcome
+  end
+end
