c3e416c60d197fdaa8d27b9e7bf19cb2a285ad02
Evan Wheeler <anon0mys87@gmail.com>
2020-03-20 16:22:57 -0600

    Add rails views back

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+
+  get '/', to: 'sessions#welcome'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'welcome', to: 'sessions#welcome'
+
+  resources :stories do
+    resources :plot_points
+  end
+
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
